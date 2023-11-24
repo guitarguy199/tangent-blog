7c3f251e1096ca76cd033465c55504fa9db6b71f
Austin O <guitarguy199@gmail.com>
2023-11-24 11:49:58 -0700

    initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Navbar() {
+
+    return (
+        <div className="max-w-3xl mx-auto sm:px-6 lg: px-8">
+            <div className="flex justify-between h-16">
+                <div className="flex justify-between items-center w-full">
+                    <Link href="/">
+                        <h1 className="text-2xl" font-medium>
+                            Tangent Apps <span className="text-teal-500">Blog</span>
+                        </h1>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={` ${inter.className} bg-white text-black dark:bg-gray-900 dark:selection::bg-gray-900 dark:text-white h-full secetion:bg-gray-50`}>
+        <Navbar />
+        {children}
+        
+        </body>
     </html>
   )
 }
